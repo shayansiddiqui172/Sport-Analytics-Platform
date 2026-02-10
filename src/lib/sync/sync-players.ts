9abbebb8e1f06eb3ab9cc06c9e4d12ec2ad01e46
Shayan Siddiqui <shayansiddiqui172@gmail.com>
2026-02-09 21:48:06 -0500

    batch player database operations to process 50 at a time, avoid 500+ sequential queries

--- src/lib/sync/sync-players.ts
+++ src/lib/sync/sync-players.ts
@@ -201,88 +201,99 @@ export async function syncPlayers(
 
   console.log(`[sync-players] Fetched ${allPlayers.length} players from leaguedashplayerstats`);
 
+  // Get all valid team IDs in one query
+  const teams = await prisma.team.findMany({ select: { id: true } });
+  const validTeamIds = new Set(teams.map(t => t.id));
+
+  // Process players in batches of 50 to avoid timeout
+  const BATCH_SIZE = 50;
   let playerCount = 0;
   let statsCount = 0;
 
-  for (const p of allPlayers) {
-    const nameParts = p.PLAYER_NAME.split(" ");
-    const firstName = nameParts[0] || "";
-    const lastName = nameParts.slice(1).join(" ") || "";
+  for (let i = 0; i < allPlayers.length; i += BATCH_SIZE) {
+    const batch = allPlayers.slice(i, i + BATCH_SIZE);
+    
+    await prisma.$transaction(async (tx) => {
+      for (const p of batch) {
+        const nameParts = p.PLAYER_NAME.split(" ");
+        const firstName = nameParts[0] || "";
+        const lastName = nameParts.slice(1).join(" ") || "";
+        const teamId = validTeamIds.has(p.TEAM_ID) ? p.TEAM_ID : null;
 
-    const teamExists = await prisma.team.findUnique({
-      where: { id: p.TEAM_ID },
+        await tx.player.upsert({
+          where: { id: p.PLAYER_ID },
+          update: {
+            firstName,
+            lastName,
+            teamId,
+          },
+          create: {
+            id: p.PLAYER_ID,
+            firstName,
+            lastName,
+            teamId,
+            height: "",
+            weight: "",
+          },
+        });
+
+        await tx.playerStats.upsert({
+          where: {
+            playerId_season: { playerId: p.PLAYER_ID, season: seasonStr },
+          },
+          update: {
+            gamesPlayed: p.GP,
+            ppg: p.PTS,
+            rpg: p.REB,
+            apg: p.AST,
+            spg: p.STL,
+            bpg: p.BLK,
+            topg: p.TOV,
+            fgPct: p.FG_PCT,
+            threePct: p.FG3_PCT,
+            ftPct: p.FT_PCT,
+            mpg: p.MIN,
+            fgm: p.FGM,
+            fga: p.FGA,
+            fg3m: p.FG3M,
+            fg3a: p.FG3A,
+            ftm: p.FTM,
+            fta: p.FTA,
+            oreb: p.OREB,
+            dreb: p.DREB,
+            pf: p.PF,
+          },
+          create: {
+            playerId: p.PLAYER_ID,
+            season: seasonStr,
+            gamesPlayed: p.GP,
+            ppg: p.PTS,
+            rpg: p.REB,
+            apg: p.AST,
+            spg: p.STL,
+            bpg: p.BLK,
+            topg: p.TOV,
+            fgPct: p.FG_PCT,
+            threePct: p.FG3_PCT,
+            ftPct: p.FT_PCT,
+            mpg: p.MIN,
+            fgm: p.FGM,
+            fga: p.FGA,
+            fg3m: p.FG3M,
+            fg3a: p.FG3A,
+            ftm: p.FTM,
+            fta: p.FTA,
+            oreb: p.OREB,
+            dreb: p.DREB,
+            pf: p.PF,
+          },
+        });
+      }
     });
 
-    await prisma.player.upsert({
-      where: { id: p.PLAYER_ID },
-      update: {
-        firstName,
-        lastName,
-        teamId: teamExists ? p.TEAM_ID : null,
-      },
-      create: {
-        id: p.PLAYER_ID,
-        firstName,
-        lastName,
-        teamId: teamExists ? p.TEAM_ID : null,
-        height: "",
-        weight: "",
-      },
-    });
-    playerCount++;
-
-    await prisma.playerStats.upsert({
-      where: {
-        playerId_season: { playerId: p.PLAYER_ID, season: seasonStr },
-      },
-      update: {
-        gamesPlayed: p.GP,
-        ppg: p.PTS,
-        rpg: p.REB,
-        apg: p.AST,
-        spg: p.STL,
-        bpg: p.BLK,
-        topg: p.TOV,
-        fgPct: p.FG_PCT,
-        threePct: p.FG3_PCT,
-        ftPct: p.FT_PCT,
-        mpg: p.MIN,
-        fgm: p.FGM,
-        fga: p.FGA,
-        fg3m: p.FG3M,
-        fg3a: p.FG3A,
-        ftm: p.FTM,
-        fta: p.FTA,
-        oreb: p.OREB,
-        dreb: p.DREB,
-        pf: p.PF,
-      },
-      create: {
-        playerId: p.PLAYER_ID,
-        season: seasonStr,
-        gamesPlayed: p.GP,
-        ppg: p.PTS,
-        rpg: p.REB,
-        apg: p.AST,
-        spg: p.STL,
-        bpg: p.BLK,
-        topg: p.TOV,
-        fgPct: p.FG_PCT,
-        threePct: p.FG3_PCT,
-        ftPct: p.FT_PCT,
-        mpg: p.MIN,
-        fgm: p.FGM,
-        fga: p.FGA,
-        fg3m: p.FG3M,
-        fg3a: p.FG3A,
-        ftm: p.FTM,
-        fta: p.FTA,
-        oreb: p.OREB,
-        dreb: p.DREB,
-        pf: p.PF,
-      },
-    });
-    statsCount++;
+    playerCount += batch.length;
+    statsCount += batch.length;
+    console.log(`[sync-players] Processed batch ${Math.floor(i / BATCH_SIZE) + 1}/${Math.ceil(allPlayers.length / BATCH_SIZE)}`);
   }
 
   console.log(
